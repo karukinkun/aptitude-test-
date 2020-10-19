e3899d2f4e3659efdc164966fd4b9ae80fb01e65
k-moriuchi <karukinkun@gmail.com>
2020-10-19 22:38:51 +0900

    タイマー

--- src/components/Login/TestPage.tsx
+++ src/components/Login/TestPage.tsx
@@ -22,6 +22,7 @@ import ActionButton from 'components/common/ActionButton';
 import { API, graphqlOperation } from 'aws-amplify';
 import { getCatergory, listCatergorys, listQuizs, listTodos } from 'graphql/queries';
 import { GetCatergoryQuery, ListCatergorysQuery, ListQuizsQuery } from 'API';
+import moment from 'moment';
 
 /* material-uiコンポーネント 既存スタイルの上書き
 -------------------------------------------------------------- */
@@ -37,7 +38,7 @@ const TestPage: FC<{}> = () => {
   const lineAreaRef = useRef<HTMLInputElement | null>(null);
   const [catergory, setCatergory] = useState<any>();
   const [quizs, setQuizs] = useState<any>([]);
-  const [time, setTime] = useState<any>(100);
+  const [time, setTime] = useState<any>(300000);
 
   const [conditions, setConditions] = useState<any>({
     question1: null,
@@ -71,56 +72,60 @@ const TestPage: FC<{}> = () => {
     })();
   }, []);
 
-  const countup = () => {
-    setTime((time: any) => time + 1);
-  };
-
-  useEffect(() => {
-    const timerId = setInterval(countup, 1000);
-
-    return () => clearInterval(timerId);
+  // カウントダウン処理
+  const countdown = useCallback(() => {
+    setTime((count: number) => count - 1000);
   }, []);
 
+  // 1秒ごとにカウントダウン処理を実行
+  useEffect(() => {
+    setInterval(countdown, 1000);
+  }, [countdown]);
+
   return (
-    <Box px={40}>
-      <Grid container alignItems="center">
-        <Typography component="p" variant="h5">
-          <Box component="span">{time}</Box>
+    <>
+      <Box bgcolor="primary.main" py={3} textAlign="center">
+        <Typography component="p" variant="h4">
+          <Box component="span" color="#FFF" fontWeight={700}>
+            {`残り時間：${moment(time).format('mm分ss秒')}`}
+          </Box>
         </Typography>
-      </Grid>
-      <Grid container justify="center" alignItems="center">
-        {quizs.map((quiz: any) => (
-          <Grid item xs={12} key={quiz.no}>
-            <Box className={pageClass.inputEmail}>
-              <Box mb={3}>
-                <Typography component="p" variant="h4">
-                  <span
-                    // 文字列に含まれる<br>タグを認識させるため、文字列をHTMLに変換
-                    // eslint-disable-next-line react/no-danger
-                    dangerouslySetInnerHTML={{
-                      __html: quiz.question,
-                    }}
-                  />
-                </Typography>
+      </Box>
+      <Box px={40}>
+        <Grid container justify="center" alignItems="center">
+          {quizs.map((quiz: any) => (
+            <Grid item xs={12} key={quiz.no}>
+              <Box className={pageClass.inputEmail}>
+                <Box mb={3}>
+                  <Typography component="p" variant="h4">
+                    <span
+                      // 文字列に含まれる<br>タグを認識させるため、文字列をHTMLに変換
+                      // eslint-disable-next-line react/no-danger
+                      dangerouslySetInnerHTML={{
+                        __html: quiz.question,
+                      }}
+                    />
+                  </Typography>
+                </Box>
+                <FormControl component="fieldset">
+                  <StyledRadioGroup name="question" onChange={handleChange}>
+                    {quiz.choicesA && <FormControlLabel value={0} control={<Radio />} label={quiz.choicesA} />}
+                    {quiz.choicesB && <FormControlLabel value={1} control={<Radio />} label={quiz.choicesB} />}
+                    {quiz.choicesC && <FormControlLabel value={2} control={<Radio />} label={quiz.choicesC} />}
+                    {quiz.choicesD && <FormControlLabel value={3} control={<Radio />} label={quiz.choicesD} />}
+                  </StyledRadioGroup>
+                </FormControl>
               </Box>
-              <FormControl component="fieldset">
-                <StyledRadioGroup name="question" onChange={handleChange}>
-                  {quiz.choicesA && <FormControlLabel value={0} control={<Radio />} label={quiz.choicesA} />}
-                  {quiz.choicesB && <FormControlLabel value={1} control={<Radio />} label={quiz.choicesB} />}
-                  {quiz.choicesC && <FormControlLabel value={2} control={<Radio />} label={quiz.choicesC} />}
-                  {quiz.choicesD && <FormControlLabel value={3} control={<Radio />} label={quiz.choicesD} />}
-                </StyledRadioGroup>
-              </FormControl>
-            </Box>
+            </Grid>
+          ))}
+          <Grid item xs={4}>
+            <ActionButton fullWidth color="primary">
+              送信
+            </ActionButton>
           </Grid>
-        ))}
-        <Grid item xs={4}>
-          <ActionButton fullWidth color="primary">
-            送信
-          </ActionButton>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 };
 export default TestPage;
